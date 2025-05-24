50347e551339cfee18af3dd1e3187390923fa689
KaedeZzz <zhanfengzhou0408@gmail.com>
2025-05-24 12:39:48 +0100

    Plots and type declaration tweaks for assignment scripts

--- scripts/assignment_1-7.py
+++ scripts/assignment_1-7.py
@@ -3,17 +3,17 @@ import matplotlib.pyplot as plt
 from src.graph_methods.graph_gen import random_graph
 from src.graph_methods.graph_stats import get_reachable
 
-n = 4096
-p_list = np.linspace(10**-5, 10**-3, 20)
-repeat = 20
+n: int = 4096
+p_list: np.array = np.linspace(10**-5, 10**-3, 20)
+repeat: int = 20
 
-p_crit = 1 / (n - 1)
-reach_list = []
+p_crit: float = 1 / (n - 1)
+reach_list: list[float] = []
 
 print("p_crit = 1/(n - 1) = {0:.8f}".format(p_crit))
 
 for p in p_list:
-    reach = 0
+    reach: float = 0.0
     for _ in range(repeat):
         network = random_graph(n, p, method='two-step')
         reach += len(get_reachable(network))
@@ -23,6 +23,5 @@ for p in p_list:
 
 plt.plot(p_list, np.log(np.array(reach_list)))
 plt.vlines(x=p_crit, ymin=0, ymax=np.max(np.log(np.array(reach_list))), colors='r', label='p_crit')
-plt.title("log-average count of reachable node vs. generation probability")
 plt.legend()
 plt.show()
