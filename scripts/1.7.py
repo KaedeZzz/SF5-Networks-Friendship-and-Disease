b66e3314426df77e830aebe8566b21ad8d3a36f5
KaedeZzz <zhanfengzhou0408@gmail.com>
2025-06-03 04:33:51 +0100

    [feat](SIR) Framework for SIR graph subclass

--- scripts/1.7.py
+++ scripts/1.7.py
@@ -3,25 +3,27 @@ import matplotlib.pyplot as plt
 from src.graph_methods.graph_gen import random_graph
 from src.graph_methods.graph_stats import get_reachable
 
-n: int = 4096
-p_list: np.array = np.linspace(10**-5, 10**-3, 20)
-repeat: int = 20
 
-p_crit: float = 1 / (n - 1)
-reach_list: list[float] = []
+if __name__ == '__main__':
+    n: int = 4096
+    p_list: np.array = np.linspace(10**-5, 10**-3, 20)
+    repeat: int = 20
 
-print("p_crit = 1/(n - 1) = {0:.8f}".format(p_crit))
+    p_crit: float = 1 / (n - 1)
+    reach_list: list[float] = []
 
-for p in p_list:
-    reach: float = 0.0
-    for _ in range(repeat):
-        network = random_graph(n, p, method='two-step')
-        reach += len(get_reachable(network))
-    reach /= repeat
-    print("Average number of reachable nodes for p = {0:.8f}: {1:.2f}".format(p, reach))
-    reach_list.append(reach)
+    print("p_crit = 1/(n - 1) = {0:.8f}".format(p_crit))
 
-plt.plot(p_list, np.log(np.array(reach_list)))
-plt.vlines(x=p_crit, ymin=0, ymax=np.max(np.log(np.array(reach_list))), colors='r', label='p_crit')
-plt.legend()
-plt.show()
+    for p in p_list:
+        reach: float = 0.0
+        for _ in range(repeat):
+            network = random_graph(n, p, method='two-step')
+            reach += len(get_reachable(network))
+        reach /= repeat
+        print("Average number of reachable nodes for p = {0:.8f}: {1:.2f}".format(p, reach))
+        reach_list.append(reach)
+
+    plt.plot(p_list, np.log(np.array(reach_list)))
+    plt.vlines(x=p_crit, ymin=0, ymax=np.max(np.log(np.array(reach_list))), colors='r', label='p_crit')
+    plt.legend()
+    plt.show()
