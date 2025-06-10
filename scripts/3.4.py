fd8ffaf077d4a0fb3b7d5f9193c5bfb936b463b5
KaedeZzz <zhanfengzhou0408@gmail.com>
2025-06-10 14:10:18 +0100

    task: assignment 3.4

---
+++ scripts/3.4.py
@@ -0,0 +1,21 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+from src.io import load_graph, save_dir
+from src.models import non_infected_cluster_size
+
+path = save_dir / 'rg_n10000_d20_p.pkl'
+
+if __name__ == '__main__':
+    graph = load_graph(path)
+    rate_ind_list = np.linspace(-2, -1, 20)
+    transfer_rate_list = np.power(10, rate_ind_list)
+    coeff_of_var_list = []
+    for i, rate in enumerate(transfer_rate_list):
+        dist = non_infected_cluster_size(graph, rate, repeat=50)
+        mean = np.mean(dist)
+        std = np.std(dist)
+        coeff_of_var_list.append(std/mean)
+        print(f"sample collected from simulation with rate {i + 1}, {len(transfer_rate_list)} in total")
+    plt.plot(rate_ind_list, coeff_of_var_list)
+    plt.show()
